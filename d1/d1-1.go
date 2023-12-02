a8db41032cf17d9a47b8fa3c33b4ebbacb4741e2
nealwp <mrprestonneal@gmail.com>
2023-12-01 21:53:08 -0800

    another test passes

--- d1/d1-1.go
+++ d1/d1-1.go
@@ -2,20 +2,29 @@ package d1
 
 import "strconv"
 
-func CalibrationValue(in string) (int, error) {
-    if len(in) == 1 {
-        i, err := strconv.Atoi(in + in)
+func CalibrationValue(input string) (int, error) {
+
+    var numStr []string
+
+    for _, char := range input {
+        if _, err := strconv.Atoi(string(char)); err == nil {
+            numStr = append(numStr, string(char))
+        }
+    }
+
+    if len(numStr) == 1 {
+        i, err := strconv.Atoi(numStr[0] + numStr[0]) 
+        if err != nil {
+            panic(err)
+        }
+        return i, nil
+    } else {
+        i, err := strconv.Atoi(numStr[0] + numStr[1])
         if err != nil {
             panic(err)
         }
         return i, nil
     }
-
-    i, err := strconv.Atoi(in) 
-    if err != nil {
-        panic(err)
-    }
-    return i, nil
 
 }
 
