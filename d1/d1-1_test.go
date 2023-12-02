812e39b4a6c8507c70318986f794f34fdadcb9d7
nealwp <mrprestonneal@gmail.com>
2023-12-01 21:27:56 -0800

    first test pass

--- d1/d1-1_test.go
+++ d1/d1-1_test.go
@@ -4,12 +4,24 @@ import (
      "testing"
 ) 
 
+type testCase struct {
+    val string
+    want int
+}
+
+
+var testCases = []testCase {
+    {"3", 33},
+    {"12", 12},
+}
+
+
 func TestCalibrationValue(t *testing.T) {
-    value := "3"
-    want := 33 
-    res, err := CalibrationValue(value)
-    if want != res || err != nil {
-        t.Fatalf(`got %v, %v, want match for %v, nil`, res, err, want)
+    for _, test := range testCases {
+        res, err := CalibrationValue(test.val)
+        if test.want != res || err != nil {
+            t.Fatalf(`got %v, %v, want match for %v, nil`, res, err, test.want)
+        }
     }
 }
 
