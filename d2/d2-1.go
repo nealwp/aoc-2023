784eac40658298d9f74dee75dd2073fc34afb809
Preston Neal <mrprestonneal@gmail.com>
2023-12-02 14:32:56 -0800

    d2-1 tests passing

--- d2/d2-1.go
+++ d2/d2-1.go
@@ -13,21 +13,66 @@ type Handful struct {
 
 type Game struct {
     Id int
-    Hands []Handful
+    Possible bool
 }
 
-func SumPossibleGameIds(input string) (int, error) {
-    return 0, nil
+func SumPossibleGameIds(input []string) (int, error) {
+    var sum int
+    for _, s := range input {
+        sum += ParseGameString(s)
+    }
+
+    return sum, nil
 }
 
-func ParseGameString(input string) (Game, error) {
+func ParseGameString(input string) (int) {
     var game Game
-    s := strings.Split(input, " ")
+    game.Possible = true
+
     // id is always second word in string
-    id, err := strconv.Atoi(strings.Split(s[1], ":")[0])
+    s := strings.Split(input, ":")
+    gameStr := s[0]
+
+    id, err := strconv.Atoi(strings.Split(gameStr, " ")[1])
     if err != nil {
         panic(err)
     }
     game.Id = id
-    return game, nil 
+
+    handsStr := s[1]
+    hands := strings.Split(handsStr, ";")
+    
+    for _, h := range hands {
+        var hand Handful
+        for _, e := range strings.Split(h, ",") {
+            blueIdx := strings.Index(e, "blue")
+            if blueIdx != -1 {
+                blueNum := strings.TrimSpace(strings.Split(e, "blue")[0])
+                hand.Blue, err = strconv.Atoi(blueNum)
+            }
+
+            redIdx := strings.Index(e, "red")
+            if redIdx != -1 {
+                redNum := strings.TrimSpace(strings.Split(e, "red")[0])
+                hand.Red, err = strconv.Atoi(redNum)
+            }
+
+            greenIdx := strings.Index(e, "green")
+            if greenIdx != -1 {
+                greenNum := strings.TrimSpace(strings.Split(e, "green")[0])
+                hand.Green, err = strconv.Atoi(greenNum)
+            }
+        }
+
+        if hand.Blue > 14 || hand.Red > 12 || hand.Green > 13 {
+            game.Possible = false
+            break
+        }
+    }
+
+    if game.Possible {
+        return game.Id
+    } else {
+        return 0
+    }
 }
--- d2/d2-1_test.go
+++ d2/d2-1_test.go
@@ -5,13 +5,14 @@ import (
 ) 
 
 func TestSumPossibleGameIds(t *testing.T) {
-    input := `
-        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    `
+    input := []string{
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    }
+
     want := 8
     res, err := SumPossibleGameIds(input)
     if res != want || err != nil {
@@ -19,19 +20,23 @@ func TestSumPossibleGameIds(t *testing.T) {
     }
 }
 
+type testCase struct {
+    test string
+    want int
+}
+
 func TestParseGameString(t *testing.T) {
-    input := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green;"
-    want := Game{
-        Id: 1,
-        Hands: []Handful{
-            {Blue: 3, Red: 4, Green: 0},
-            {Blue: 6, Red: 1, Green: 2},
-            {Blue: 0, Red: 0, Green: 2},
-        },
+    testCases := []testCase{
+        {"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1},
+        {"Game 2: 3 blue, 20 red; 1 red, 2 green, 6 blue; 2 green", 0},
     }
 
-    res, err := ParseGameString(input)
-    if res.Id != want.Id || err != nil {
-        t.Errorf(`got %v, %v, want match for %v, nil`, res.Id, err, want.Id)
+    for _, i := range testCases {
+        res := ParseGameString(i.test)
+        if res != i.want {
+            t.Errorf(`got %v, want match for %v`, res, i.want)
+        }
     }
+
+    
 }
