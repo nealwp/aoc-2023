1478028c12c3e4c79c753e2c2f3dc5062f6bccc6
Preston Neal <mrprestonneal@gmail.com>
2023-12-05 16:02:50 -0800

    wip d4-2

--- d4/d4-2.go
+++ d4/d4-2.go
@@ -1,5 +1,91 @@
 package d4
 
+import (
+	"fmt"
+	"strings"
+)
+
+type Card struct {
+    matches int
+    copies int
+}
+
 func SumScratchCards(input []string) (int, error) {
-    return 0, nil
+    var sum int
+    var cards []Card
+
+    for _, line := range input {
+        card := GetWinningScratchCards(line)
+        cards = append(cards, card)
+    }
+    
+    mutCards := SetMults(cards)
+
+    fmt.Println(cards)
+    for _, card := range mutCards {
+        sum += card.copies
+    }
+
+    return sum, nil
+}
+
+func GetWinningScratchCards(input string) Card {
+    var numWinners int
+    holderMap := make(map[string]int)
+
+    cards := strings.Split(strings.Split(input, ": ")[1], " | ")
+    winners := strings.Split(cards[0], " ")
+    holders := strings.Split(cards[1], " ")
+
+    for _, v := range holders {
+        if v != "" {
+            holderMap[strings.TrimSpace(v)] = 0 
+        }
+    }
+
+    for _, w := range winners {
+        if _, found := holderMap[strings.TrimSpace(w)]; found {
+            numWinners += 1
+        }
+    }
+
+    card := Card{
+        matches: numWinners,
+        copies: 1,
+    }
+
+    return card
+}
+
+func SetMults(cards []Card) []Card {
+    
+    // for number of matches
+    // multiply copies * matches for each card
+
+    // bump the number of copies for next cards
+
+
+    for i, card := range cards {
+        m := 1 
+        for m <= card.matches {
+
+            if i + m >= len(cards) {
+                break
+            }
+
+            next := cards[i + m]
+            next.copies += 1
+
+            m++
+        }
+
+
+        j := 1
+        for j < len(cards) {
+            c := cards[j] 
+            c.matches = c.copies * c.matches 
+            j++
+        }
+    }
+    return cards
 }
--- d4/d4-2_test.go
+++ d4/d4-2_test.go
@@ -4,16 +4,66 @@ import "testing"
 
 func TestSumScratchCards(t *testing.T) {
     input := []string{
-        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", // 4
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", // 2 + 1
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", // 2 + 1 + 1
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", // 1 + 1 + 1 + 1 
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", // 0 + 1     + 1 + 1
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", // 0
     }
     want := 30 
     res, err := SumScratchCards(input)
     if res != want || err != nil {
-        t.Fatalf("got: %v, %v wanted: %v, nil", res, err, want)
+        t.Errorf("got: %v, %v wanted: %v, nil", res, err, want)
+    }
+}
+
+type testCase struct {
+    val string
+    want Card
+}
+
+func TestGetWinningScratchCards(t *testing.T) {
+    tests := []testCase{
+        {"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", Card{matches: 4, copies: 1}},
+        {"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", Card{matches: 2, copies: 1}},
+        {"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", Card{matches: 2, copies: 1}},
+        {"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", Card{matches: 1, copies: 1}},
+        {"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", Card{matches: 0, copies: 1}}, 
+        {"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", Card{matches: 0, copies: 1}},
+    }
+
+    for _, test := range tests {
+        res := GetWinningScratchCards(test.val)
+        if res != test.want {
+            t.Errorf("got %v, wanted %v", res, test.want)
+        }
+    }
+}
+
+func TestSetMults(t *testing.T) {
+    input := []Card {
+        {matches: 4, copies: 1},
+        {matches: 2, copies: 1},
+        {matches: 2, copies: 1},
+        {matches: 1, copies: 1},
+        {matches: 0, copies: 1}, 
+        {matches: 0, copies: 1},
+    }
+    
+    want := []Card {
+        {matches: 4, copies: 1},
+        {matches: 2, copies: 2},
+        {matches: 2, copies: 4},
+        {matches: 1, copies: 8},
+        {matches: 0, copies: 14}, 
+        {matches: 0, copies: 1},
+    }
+
+    res := SetMults(input)
+    for i, r := range res {
+        if r != want[i] {
+            t.Errorf("want %v, got %v", want[i], r)
+        }
     }
 }
